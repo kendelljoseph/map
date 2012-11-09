9f59ab6467eff97440c2211ef548901f6478a589
Kendell Joseph <contact@kendelljoseph.com>
2012-11-09 17:56:31 -0500

    Added icons, simplified code, began UI construction

--- app/js/init.js
+++ app/js/init.js
@@ -1,61 +1,85 @@
 // Init
 
 $(document).ready(function(){
-    var doc = $(document);
-    var client = new Client();
+    var doc = $(document);      // Default document element
+    var body = $(document.body);// Default client body element
+    var M = Modernizr;          // Redefine Modernizr Library
+    var client = new Client();  // client is resolved from the Client constructor
     
 	function Client(){
-        var opts = arguments[0];
-        this.xMax       = opts.width == undefined ? doc.width() :  opts.width;     // Default client width is the document width
-        this.yMax       = doc.height(); // Default client height is the document height
-        this.touch      = Modernizr.touch;
-        this.canvas     = Modernizr.canvas;
-        this.geolocation= Modernizr.geolocation;
+        var self = this;
+            self.ui = {};
         
+        self.xMax       = doc.width();  // Set the maximum x value
+        self.yMax       = doc.height(); // Set the maximum y value
+        self.mobile     = M.touch;      // Detect touch, if the client is touch friendly, it is assumed mobile
+        self.canvas     = M.canvas;     // Detect HTML5 canvas
+        self.geolocation= M.geolocation;// Detect geolocation
         
-		// Detect things the app needs to know about the client (device type, gps, dimentions etc)
-		// bind client side events and attributes to "this" object
+        self.Status = function(){
+            
+            /* The image source in the status-bar is based on device and client attributes */
+            var deviceTypeImgSrc = self.mobile ?
+                    "images/icons/default/black/mobile.png" : 
+                    "images/icons/default/black/computer.png",
+                canvasTypeSrc = self.canvas ? 
+                    "images/icons/default/black/flower.png" : 
+                    "images/icons/default/red/flower.png";
+            
+            /* Create the image elements and set their sources */
+            var deviceType = $('<img />')
+                    .attr('src', deviceTypeImgSrc),
+                canvasType = $('<img />')
+                    .attr('src', canvasTypeSrc);
+            
+            /* Create the status bar, set it's default class, and append the image elements */
+            self.ui.status = $('<div />')
+                .css("display", "none")
+                .addClass("default_ui")
+                .append(deviceType)
+                .append(canvasType);
+            
+            body.append(self.ui.status);
+                self.ui.status.fadeIn(1500);    
+            
+            
+            return self.ui.status;
+        };
+        
+        self.Sample = function(){
+            self.ui.sample = $('<div />')
+                .css("display", "none")
+                .attr("id", "sample")
+                .addClass("default_ui")
+                .css({
+                    width: self.xMax - 40,
+                    height: self.yMax - 140
+                });
+            
+            body.append(self.ui.sample);
+                self.ui.sample.fadeIn(1500);
+            
+            
+            return self.ui.sample;
+        };
+        
+        self.OpenLayersMap = function(){
+            // Check for dependant resources
+            // Add dependant resources
+            // continue
+        };
+        
+        if(self.mobile){
+            // Disable overscrolling on mobile devices (iPad/Android for now)
+            doc.bind("touchstart", function(e){ 
+                e.preventDefault(); 
+            });
+        }
 	}
-	
-	function execGreeting(){
-		var hideTime = arguments[0];
-		// Show a greeting based on device type
-		// Remove all greeting elements ( to reduce clutter )
-	}
-	
-	function execUI(){
-		// Create UI elements based on client dimentions
-		// Place UI elements at a position based on client dimentions
-	}
-	
-	function execUIEvents(){
-		// Create events based on client type
-		// Bind elements to corrisponding UI elements
-	}
-	
-	/* 
-	// BACK BURNERS
-	
-	function execGoogleMap(){
-		var opts = arguments[0];
-		// Check for dependant resources
-		// Add dependant resources
-		// continue
-	}
-	
-	function execOpenLayersMap(){
-		var opts = arguments[0];
-		// Check for dependant resources
-		// Add dependant resources
-		// continue
-	}
-	*/
 
-
-	execGreeting(2000);		// Create and show a greeting, accepts an argument to change the the time before fading out in miliseconds.
-    execUI();               // Create user interface elements and controls
-	execUIEvents();         // Bind interface elements to their function
+    var statusBar = client.Status(), // Create user interface elements and controls
+        sample = client.Sample();
     
     // Debugging
-    console.log(client);
+    console.log(client, statusBar, sample);
 });
