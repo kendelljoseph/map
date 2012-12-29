1998e0cd752c820863f52d2d9cddd457ce0da211
Kendell Joseph <contact@kendelljoseph.com>
2012-12-28 20:51:24 -0500

    Init

--- app/js/init.js
+++ app/js/init.js
@@ -3,6 +3,7 @@
 $(document).ready(function(){
     var doc = $(document);      // Default document element
     var body = $(document.body);// Default client body element
+    var drawing = false;
     var M = Modernizr;          // Redefine Modernizr Library
     var client = new Client();  // client is resolved from the Client constructor
     
@@ -54,11 +55,11 @@ $(document).ready(function(){
         self.Canvas = function(){
             self.ui.canvas = $('<canvas />')
                 .css("display", "none")
-                .attr("id", "sample")
+                .attr("id", "senselab")
                 .addClass("default_ui")
                 .attr("width", self.xMax - 40)
-                .attr("height", self.yMax - 140);
-            
+                .attr("height", self.yMax - 90);
+
             body.append(self.ui.canvas);
                 self.ui.canvas.fadeIn(1500);
                 
@@ -80,8 +81,48 @@ $(document).ready(function(){
 	}
 
     var statusBar = client.Status(), // Create user interface elements and controls
-        sample = client.Canvas();
+        senselab = client.Canvas();
     
-    // Debugging
-    console.log(client, statusBar, sample);
+    
+    /* 
+        Canvas Manipulation
+    */
+    senselab.bind("mousedown", function(e){
+        var pixelCount = 0;
+        var red = 0;
+        var green = 0;
+        var blue = 0;
+        
+        $(this).setPixels({
+            x: e.offsetX-9, y: e.offsetY-9,
+            width: 1, height: 1,
+            // loop through each pixel
+            each: function(px) {
+                
+                red   += px.r;
+                green += px.g;
+                blue  += px.b;
+                pixelCount += 1;
+                
+                px.r = 255 - px.r;
+                px.g = 255 - px.g;
+                px.b = 255 - px.b;
+                
+            }
+        });
+        
+        red = Math.ceil(red/pixelCount);
+        green = Math.ceil(green/pixelCount);
+        blue = Math.ceil(blue/pixelCount);
+        
+        var rgbString = "rgb(" + red + "," + blue + "," + green + ")";
+        $(document.body).css("backgroundColor", rgbString);
+        console.log([red, blue, green]);
+    });
+    
+    senselab.drawImage({
+        source: "images/nasa/greatdivide.png",
+        x: 600, y: 310,
+        height: 600, width: 1117
+    });
 });
